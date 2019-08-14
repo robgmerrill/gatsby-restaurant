685a52c1ddddae13ca48784d9e228de194966334
Rob Merrill <robgmerrill@gmail.com>
2019-08-14 13:35:11 -0700

    Add about contact and menu pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+    <Layout>
+        <SEO title="Home" />
+        <h3>This is an about page</h3>
+    </Layout>
+)
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+    <Layout>
+        <SEO title="Home" />
+        <h3>This is contact page</h3>
+    </Layout>
+)
+
+export default ContactPage
